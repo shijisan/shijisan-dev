8e8ca6d6bdfa917c3b3e5d86a5223cadfd72d7f4
shijisan <cjsantos2411638@gmail.com>
2025-06-10 04:09:23 +0800

    main, dev ver. made progress on landing page (header, tech stack section, nav bar)

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,49 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+export default function Landing() {
+	return (
+		<>
+			<main>
+				<header className="rounded-b-[5rem] md:h-[80vh] bg-gradient-to-r from-foreground to-foreground/85 flex items-center justify-center">
+					<div className="flex items-center justify-center w-full h-full flex-col max-w-[455px]">
+						<div>
+							<div className="rounded-full w-fit p-2 bg-background text-center relative top-2/5 -right-4/5 -rotate-12">
+								<h1 className="text-xs">Web Developer 🧑‍💻</h1>
+							</div>
+							<Image className="rounded-full mb-8 border-4 border-background/10" width={200} height={200} src="/profile.avif" alt="" />
+						</div>
+						<h1 className="text-4xl text-background font-satoshi text-center font-medium">Building the future<br />one line at a time.</h1>
+						{/* <p className="text-neutral-300 text-xs text-center mb-8">
+							<span className="text-blue-400">Full-Stack Web Developer</span> powered by development and innovation, specializing in Next JS, Node JS, Typescript, Javascript, React Js, Rest APIs, Tailwind CSS.
+						</p> */}
+						<Link href="/" className="mt-8 rounded-full px-8 py-2 text-sm hover:brightness-95 active:opacity-95 text-white bg-blue-500 font-satoshi font-medium border-0 shadow-sm">Let's Talk 🤝</Link>
+					</div>
+				</header>
+				<section className="bg-foreground">
+					<div className="rounded-b-[5rem] bg-background">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+						<div className="items-center justify-evenly text-foreground/50 text-7xl grid grid-cols-5 justify-items-center py-16 max-w-7xl mx-auto gap-4">
+							<i className="devicon-nextjs-original-wordmark"></i>
+							<i className="devicon-nodejs-plain-wordmark"></i>
+							<i className="devicon-typescript-plain text-4xl"></i>
+							<i className="devicon-javascript-plain text-4xl"></i>
+							<i className="devicon-react-original-wordmark text-5xl"></i>
+							<i className="devicon-postgresql-plain-wordmark text-5xl"></i>
+							<i className="devicon-prisma-original-wordmark"></i>
+							<i className="devicon-python-plain-wordmark text-5xl"></i>
+							<i className="devicon-fastapi-plain-wordmark"></i>
+							<i className="devicon-tensorflow-original-wordmark"></i>
+
+						</div>
+					</div>
+				</section>
+				<section className="bg-foreground min-h-screen">
+
+				</section>
+			</main>
+		</>
+	)
 }
